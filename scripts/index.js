118462976c78ee75dfa5656fdb9fa7f300c69db6
zmarks00 <122951066+zmarks00@users.noreply.github.com>
2025-01-09 14:27:34 -0500

    project 4 stage 3 submission

--- scripts/index.js
+++ scripts/index.js
@@ -26,3 +26,20 @@ const initialCards = [
 ];
 
 console.log(initialCards);
+
+const profileEditButton = document.querySelector(".profile__edit-button");
+
+const editModal = document.querySelector("#edit-modal");
+const editModalCloseButton = editModal.querySelector(".modal__close-button");
+
+function openModal() {
+  editModal.classList.add("modal_opened");
+}
+
+function closeModal() {
+  editModal.classList.remove("modal_opened");
+}
+
+profileEditButton.addEventListener("click", openModal);
+
+editModalCloseButton.addEventListener("click", closeModal);
